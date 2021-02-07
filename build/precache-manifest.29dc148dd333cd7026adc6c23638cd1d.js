2bd4619858bf37722c6c0b8eccaba40a043ad664
Jeremias ramirez <jereramirez499@gmail.com>
2021-02-06 23:38:31 -0400

    Agregando build

---
+++ build/precache-manifest.29dc148dd333cd7026adc6c23638cd1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99cefb448a0bc107d3ac1b00d4248ec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da956ccda7792d26d8ed",
+    "url": "/static/css/main.46bf85de.chunk.css"
+  },
+  {
+    "revision": "01fd5990410ad87cc9d4",
+    "url": "/static/js/2.c17d86d9.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/static/js/2.c17d86d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da956ccda7792d26d8ed",
+    "url": "/static/js/main.6327c213.chunk.js"
+  },
+  {
+    "revision": "7307d4920a60a3250bd6",
+    "url": "/static/js/runtime-main.1b4d1555.js"
+  },
+  {
+    "revision": "b4a359b4af19fadf044dbd201bf390eb",
+    "url": "/static/media/logo-start-app.b4a359b4.svg"
+  }
+]);
